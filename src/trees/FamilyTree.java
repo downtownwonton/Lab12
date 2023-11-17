c671485ae7ef7d026d5302425e52f72badffd39e
Vincent <alisasis.vincent@gmail.com>
2023-11-17 13:06:22 -0800

    updated methods. need to bug catch

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -43,7 +43,7 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (name.equals(targetName))
+            if (this.name.equals(targetName))
                 return this;
                     
             // Now, recurse. Check all children of this node.
@@ -65,13 +65,12 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
-            for (TreeNode child : children)
             {
-            	while (child.parent != null)
+            	TreeNode current = this.parent;
+            	while (current != null)
             	{
-            		ancestors.add(child.parent);
-            		child = child.parent;
-            		
+            		ancestors.add(current);
+            		current = current.parent;
             	}
             }
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
@@ -121,8 +120,8 @@ public class FamilyTree
 
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
+		FileReader fr = new FileReader(treeFile);
+		BufferedReader br = new BufferedReader(fr);
 		String line;
 		while ((line = br.readLine()) != null)
 			addLine(line);
@@ -138,31 +137,40 @@ public class FamilyTree
 	private void addLine(String line) throws TreeException
 	{
 		// Extract parent and array of children.
-		int colonIndex = ?? should be the index of the colon in line.
+		int colonIndex = line.indexOf(":"); // should be the index of the colon in line.
 		if (colonIndex < 0)
-			?? throw a TreeException with a useful message
-		String parent = ?? The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
-				           class java.util.String, method substring(), if you need guidance.
-		String childrenString = ?? The substring of line that starts just after colonIndex and goes through the end of
-				                   the line. You'll use a different version of substring().
-		String[] childrenArray = ?? Call childrenString.split(). Check the API for details. The result will be an array
-				                    of strings, with the separating commas thrown away.
+			 throw new TreeException("no.");
+		String parent = line.substring(colonIndex); //The substring of line that starts at char #0 and ends just before colonIndex. Check the API for 
+				           //class java.util.String, method substring(), if you need guidance.
+		String childrenString = line.substring(colonIndex, line.length()); //The substring of line that starts just after colonIndex and goes through the end of
+				                   //the line. You'll use a different version of substring().
+		String[] childrenArray = childrenString.split(","); //Call childrenString.split(). Check the API for details. The result will be an array
+				                    //of strings, with the separating commas thrown away.
 		
 		// Find parent node. If root is null then the tree is empty and the
 		// parent node must be constructed. Otherwise the parent node should be 
 		// somewhere in the tree.
 		TreeNode parentNode;
 		if (root == null)
-			parentNode = root = new TreeNode(parent);
+		{
+			root = new TreeNode(parent);
+			parentNode = root;
+		}
+			
 		else
 		{
-			parentNode = root.?????  There's a method in Node that searches for a named node. 
-			??? If the parent node wasn't found, there must have been something wrong in the 
-				data file. Throw an exception.
+			parentNode = root.getNodeWithName(parent);
+			if (parentNode == null) throw new TreeException("no.");//There's a method in Node that searches for a named node. 
+			//??? If the parent node wasn't found, there must have been something wrong in the 
+				//data file. Throw an exception.
 		}
-		
+		for (int i = 0; i < childrenArray.length; i++)
+		{
+			TreeNode child = new TreeNode(childrenArray[i]);
+			parentNode.children.add(child);
+		}
 		// Add child nodes to parentNode.
-		?? For each name in childrenArray, create a new node and add that node to parentNode.
+		//?? For each name in childrenArray, create a new node and add that node to parentNode.
 	}
 	
 	
@@ -175,16 +183,16 @@ public class FamilyTree
 	TreeNode getMostRecentCommonAncestor(String name1, String name2) throws TreeException
 	{
 		// Get nodes for input names.
-		TreeNode node1 = root.???		// node whose name is name1
+		TreeNode node1 = root.getNodeWithName(name1);		// node whose name is name1
 		if (node1 == null)
-			??? Throw a TreeException with a useful message
-		TreeNode node2 = root.???		// node whose name is name2
+			throw new TreeException("no node with " + name1);
+		TreeNode node2 = root.getNodeWithName(name2);		// node whose name is name2
 		if (node2 == null)
-			??? Throw TreeException with a useful message
+			throw new TreeException("no node with " + name2);
 		
 		// Get ancestors of node1 and node2.
-		ArrayList<TreeNode> ancestorsOf1 = ???
-		ArrayList<TreeNode> ancestorsOf2 = ???
+		ArrayList<TreeNode> ancestorsOf1 = node1.collectAncestorsToList();
+		ArrayList<TreeNode> ancestorsOf2 = node2.collectAncestorsToList();
 		
 		// Check members of ancestorsOf1 in order until you find a node that is also
 		// an ancestor of 2. 
@@ -212,13 +220,14 @@ public class FamilyTree
 			TreeNode ancestor = tree.getMostRecentCommonAncestor("Bilbo", "Frodo");
 			System.out.println("Most recent common ancestor of Bilbo and Frodo is " + ancestor.getName());
 		}
-		catch (IOException x)
-		{
-			System.out.println("IO trouble: " + x.getMessage());
-		}
 		catch (TreeException x)
 		{
 			System.out.println("Input file trouble: " + x.getMessage());
 		}
+		catch (IOException x)
+		{
+			System.out.println("IO trouble: " + x.getMessage());
+		}
+		
 	}
 }
